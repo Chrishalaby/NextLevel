5858dfa9ffc69815e3d041264f4ca4f0ac4ccb95
amirhilal044 <amiramirhilal@gmail.com>
2024-02-06 20:19:15 +0200

    adding show about us and logo and enums

---
+++ src/app/shared/enums/backend.enum.ts
@@ -0,0 +1,14 @@
+export enum BackendControllerRoute {
+  User = '/users',
+  Trainer = '/trainer',
+}
+
+export enum TrainerParams {
+  Update = '/update',
+  Show = '/show',
+}
+
+export enum UserParams {
+  Verify = '/verify',
+  CreateGhostClient = '/create-ghost-client',
+}
