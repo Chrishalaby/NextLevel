633672fdf4520a2e824866ca8d3769cc7c77aa0f
Chris <elhalabichristian@gmail.com>
2024-02-18 15:16:53 +0200

    added change password

--- src/app/modules/auth/components/newpassword/newpassword.component.ts
+++ src/app/modules/auth/components/newpassword/newpassword.component.ts
@@ -1,54 +1,70 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
 import { PasswordModule } from 'primeng/password';
+import { NotificationsService } from 'src/app/shared/services/notifications.service';
+import { AuthRepository } from '../../shared/services/auth.repository';
 @Component({
   templateUrl: './newpassword.component.html',
   standalone: true,
-  imports: [
-    RouterModule,
-    PasswordModule,
-    ButtonModule,
-    // FormGroup,
-    // FormBuilder,
-    // Validators,
-    // ActivatedRoute,
-  ],
+  imports: [PasswordModule, ButtonModule, ReactiveFormsModule, MessagesModule],
 })
 export class NewPasswordComponent {
-  rememberMe: boolean = false;
   resetPasswordForm!: FormGroup;
-  token: string | null = null;
+  private readonly passwordRegex = new RegExp(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  );
 
   constructor(
-    private route: ActivatedRoute,
-    private formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly authRepository: AuthRepository,
+    private readonly router: Router,
+    private readonly notificationsService: NotificationsService
   ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
-      this.token = params['token']; // Extract the token from URL
-      if (!this.token) {
-        // Handle case where token is missing or invalid
-      }
-    });
-
     this.resetPasswordForm = this.formBuilder.group({
       newPassword: ['', [Validators.required]],
-      // include other form controls as needed
+      confirmPassword: ['', [Validators.required]],
     });
-
-    this.resetPasswordForm.addControl(
-      'token',
-      this.formBuilder.control(this.token)
-    );
   }
 
   onSubmit() {
-    if (this.resetPasswordForm.valid) {
-      const resetPasswordData = this.resetPasswordForm.value;
-      // Make the API call to reset the password using resetPasswordData
+    if (this.passwordMatchValidator()) {
+      this.authRepository
+        .newPassword(this.resetPasswordForm.value)
+        .subscribe(() => {
+          this.notificationsService.showSuccessMessage(
+            'Password changed successfully!'
+          );
+
+          this.router.navigate(['/']);
+        });
+    } else {
+      this.notificationsService.showErrorMessage(
+        'Password does not match / Invalid password format'
+      );
     }
+  }
+
+  passwordMatchValidator() {
+    return (
+      (this.resetPasswordForm.get('newPassword')?.value ===
+      this.resetPasswordForm.get('confirmPassword')?.value
+        ? true
+        : false) &&
+      this.passwordRegex.test(this.resetPasswordForm.get('newPassword')?.value)
+    );
+  }
+
+  onCancel() {
+    this.router.navigate(['/']);
   }
 }
