bd2cbe6a0ae61d6e1c48e5f5f56dc49fb35e9597
Chris <elhalabichristian@gmail.com>
2024-03-31 16:51:43 +0300

    Update environment.prod.ts and aboutus.component.ts

--- src/app/modules/trainer-profile/components/showing-profile/aboutus/aboutus.component.ts
+++ src/app/modules/trainer-profile/components/showing-profile/aboutus/aboutus.component.ts
@@ -1,55 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { AvatarModule } from 'primeng/avatar';
-import { AccessTokenService } from 'src/app/modules/auth/shared/services/access-token.service';
+import { CardModule } from 'primeng/card';
+import { TabViewModule } from 'primeng/tabview';
 import { Trainer } from '../../../shared/models/trainer.model';
 import { TrainerService } from './../../../shared/services/trainer.service';
 @Component({
   templateUrl: './aboutus.component.html',
   standalone: true,
-  imports: [CommonModule, AvatarModule],
+  imports: [CommonModule, AvatarModule, CardModule, TabViewModule],
 })
 export class AboutUsComponent implements OnInit {
   visibleMember: number = -1;
-  trainerProfile: Trainer = {
-    id: 0,
-    firstName: '',
-    lastName: '',
-    briefBio: '',
-    profilePicture: '',
-    specialities: [],
-    educationalBackground: {},
-    educationalLevel: '',
-    nameOfQualification: '',
-    certifications: [],
-    phoneNumber: '',
-    email: '',
-    tiktok: '',
-    instagram: '',
-    linkedin: '',
-  };
+  trainer!: Trainer;
 
-  constructor(
-    private readonly trainerService: TrainerService,
-    private readonly accessTokenService: AccessTokenService,
-    private sanitizer: DomSanitizer
-  ) {}
+  constructor(private readonly trainerService: TrainerService) {}
 
   ngOnInit(): void {
-    const userId = this.accessTokenService.getUserInfo().id;
     this.trainerService.getTrainerProfile().subscribe((trainerProfile) => {
       const parsedEducationalBackground = JSON.parse(
         trainerProfile.educationalBackground
       );
-      this.trainerProfile = {
+      this.trainer = {
         ...trainerProfile,
         educationalBackground: parsedEducationalBackground,
       };
     });
   }
-
-  // ngOnDestroy(): void {
-  //   this.subscription.unsubscribe();
-  // }
 }
--- src/environments/envonment.prod.ts
+++ src/environments/envonment.prod.ts
@@ -1,18 +1,16 @@
 import { Environment } from 'src/app/shared/models/environment.model';
 
-// export const environment: Environment = {
-//   apiUrl: 'http://16.171.151.92/api/data',
-//   production: true,
-//   defaultLanguage: 'en',
-//   apiBaseUrl: 'http://localhost:3000',
-//   imageBaseUrl: 'http://localhost:3000/uploads/',
-// };
 export const environment: Environment = {
-  apiUrl: 'https://fithub-endpoint-dsc8enhqf5bsgpe2.z02.azurefd.net',
+  apiUrl: 'http://16.171.151.92/api/data',
   production: true,
   defaultLanguage: 'en',
-  apiBaseUrl: 'https://fithub-endpoint-dsc8enhqf5bsgpe2.z02.azurefd.net',
-  imageBaseUrl:
-    'https://fithub-endpoint-dsc8enhqf5bsgpe2.z02.azurefd.net/uploads/',
+  apiBaseUrl: 'http://localhost:3000',
+  imageBaseUrl: 'http://localhost:3000/uploads/',
 };
-// https://fithub-endpoint-dsc8enhqf5bsgpe2.z02.azurefd.net
+// export const environment: Environment = {
+//   apiUrl: 'https://fithub.azurewebsites.net/',
+//   production: true,
+//   defaultLanguage: 'en',
+//   apiBaseUrl: 'https://fithub.azurewebsites.net',
+//   imageBaseUrl: 'http://my-app-label-unique.eastus.azurecontainer.io/uploads/',
+// };
