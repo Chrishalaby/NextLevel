1a49f2b8f6b8ea93125091f4be3a36863ddee31a
Chris <elhalabichristian@gmail.com>
2024-04-02 17:52:14 +0300

    Update production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 import { Environment } from 'src/app/shared/models/environment.model';
 
-export const environment: Environment = {
-  apiUrl: 'http://16.171.151.92/api/data',
-  production: true,
-  defaultLanguage: 'en',
-  apiBaseUrl: 'http://localhost:3000',
-  imageBaseUrl: 'http://localhost:3000/uploads/',
-};
 // export const environment: Environment = {
-//   apiUrl: 'https://fithub.azurewebsites.net/',
+//   apiUrl: 'http://16.171.151.92/api/data',
 //   production: true,
 //   defaultLanguage: 'en',
-//   apiBaseUrl: 'https://fithub.azurewebsites.net',
-//   imageBaseUrl: 'http://my-app-label-unique.eastus.azurecontainer.io/uploads/',
+//   apiBaseUrl: 'http://localhost:3000',
+//   imageBaseUrl: 'http://localhost:3000/uploads/',
 // };
+export const environment: Environment = {
+  apiUrl: 'https://fithub.azurewebsites.net/',
+  production: true,
+  defaultLanguage: 'en',
+  apiBaseUrl: 'https://fithub.azurewebsites.net',
+  imageBaseUrl: 'http://my-app-label-unique.eastus.azurecontainer.io/uploads/',
+};
